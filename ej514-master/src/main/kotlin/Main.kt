17bcd55cdce26312d9fb28898bfaf36429c58946
Moise-is-sleeping <123578787+Moise-is-sleeping@users.noreply.github.com>
2023-02-04 15:57:32 +0100

    Add files via upload
    final changes

---
+++ ej514-master/src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main(){
+    val person1 = Pedido()
+    person1.Order()
+    person1.total()
+
+}
+
---
+++ ej514-master/src/main/kotlin/Pedido.kt
@@ -0,0 +1,48 @@
+class Pedido {
+
+    val rice = Product("Rice",50.95)
+    val Sausages = Product("Sausages",35.5)
+    val Chicken = Product("Chicken",95.5)
+
+    var P_List = mutableListOf<Product>()
+
+    fun Order(){
+        var x = 0
+        while(x < 3){
+            print("1) Rice\n2) Sausages\n3) Chicken \n4) Order\n=> ")
+            val a = readln().toInt()
+            if(a == 1){
+                println("Rice has been added to cart")
+                P_List.add(rice)
+                x += 1
+            }
+            else if(a ==2){
+                println("Susages have been added to cart")
+                P_List.add(Sausages)
+                x += 1
+            }
+            else if(a ==3){
+                println("Chicken has been added to cart")
+                P_List.add(Chicken)
+                x += 1
+            }
+            else if(a ==4){
+                break
+            }
+            else{
+                println("Invalid Input !!")
+            }
+
+        }
+
+    }
+
+    fun total(){
+        for(item in P_List){
+            item.Summary()
+        }
+        if(P_List[0].Compare(P_List[2])){
+            println("El Primer y ultimo producto son iguales")
+        }
+    }
+}
